f3de32e93a73eb4ef95dc0441ea39590d61b1f3f
Sonia Parker <soniaparker1207@gmail.com>
2026-01-10 15:15:08 +1100

    logic of api & data

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const data = require('./data.json');
+const PORT = 3000;
+
+const cors = require('cors'); 
+
+app.use(cors());
+
+app.get('/api/all', (req, res) => {
+  res.json(data);
+});
+
+app.get('/api/destinations', (req, res) => {
+  res.json(data.destinations);
+});
+
+app.get('/api/crew', (req, res) => {
+  res.json(data.crew);
+});
+
+app.get('/api/technology', (req, res) => {
+  res.json(data.technology);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
